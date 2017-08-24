8d2d605f750eb47bb210dd899c90cbb69dbd0de1
bpsimusic <bpsimusic@gmail.com>
2017-08-23 18:06:19 -0700

    first commit

---
+++ frontend/risk.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Test from "./test";
+
+document.addEventListener("DOMContentLoaded", () => {
+	const root = document.getElementById("root");
+	ReactDOM.render(<Test/>, root);
+});
---
+++ frontend/test.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+const Test = ()=>{
+  return (
+    <div>
+      "hello"
+    </div>
+  );
+};
+
+
+export default Test;
